b9c9a416570143387ced56d61eb3a71cd80ea8c7
abeer MJ <abeeraljaafreh@gmail.com>
2019-11-19 14:39:47 +0200

    finish exercise

---
+++ firstprogramme.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+str="Abeer jaafreh"
+print(str,end=' ')
+
+#abeer jaafreh
+print("1996abeer","jaafreh"*300000,sep='\n')

fd7023a5e5e7d85b6c848c730b9b71af5c13b177
abeer MJ <abeeraljaafreh@gmail.com>
2019-11-19 15:45:10 +0200

    edit

---
+++ exercise.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 12:44:57 2019
+
+@author: AbeerJaafreh
+"""
+
+print('Hello\n','\tHello\n','\t\tHello')
+"""
+Hello
+    Hello
+        Hello
+
+"""
+print('------------------------------------')
+
+print('Orange Academy\n'*20)
+print('------------------------------------')
+"""
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+Orange Academy
+"""
+
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+print(x,y,z,w,sep="10")
+print('------------------------------------')
+#solution 1.0102.8103.0104.2
+
+
+num1=int(input('enter first number ?'))
+num2=int(input('enter second number ?'))
+print("result = ",num1*num2)
+print('------------------------------------')
+"""solution
+enter first number? 3
+enter second number? 6
+result =  18
+"""
+
+num1=int(input('enter first number '))
+num2=int(input('enter second number '))
+num3=int(input('enter 3th number '))
+num4=int(input('enter 4th number '))
+num5=int(input('enter 5th number '))
+summ=num1+num2+num3+num4+num5
+print("Avg : ",summ/5)
+print('------------------------------------')
+"""SOLUTION
+enter first number 10
+enter second number 10
+enter 3th number 10
+enter 4th number 10
+enter 5th number 10
+
+Avg :  10.0
+"""
+x=1
+y=2.8
+z=1j
+o="orange"
+A=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(A))
+print('------------------------------------')
+"""SOLUTION
+<class 'int'>
+<class 'float'>
+<class 'complex'>
+<class 'str'>
+<class 'bool'>
+"""
+
+
+
+x,y="Orange",1
+X1,y1=100,-10
+print(x)
+print(y)
+print(X1)   #error
+print(y1)
+print('------------------------------------')
+"""SOLUTION
+Orange
+1
+100
+-10
+"""
+5x=10  #error
+print(5x) #error
+X_q$="Orange" #error
+print(X_q$)#error
+A B=14#error
+print(A B)#error
+Print("10"*10) #error
+print('------------------------------------')
+
+
+x,y="Orange",1
+X1,y1=100,-10
+print(x)
+print(y)
+print(X1)   #error
+print(y1)
+print('------------------------------------')
+"""SOLUTION
+Orange
+1
+100
+-10
+"""
+
+
+x5=10  
+print(x5) 
+X_q="Orange" 
+print(X_q)
+AB=14
+print(AB)
+print("10"*10)
+print('------------------------------------')
+"""SOLUTION
+10
+Orange
+14
+10101010101010101010
+"""
+
+#print("Hello, World!")
+print("Cheers,Mate!") #this is the programme
+#Testing
+#The Code
+print('------------------------------------')
+"""SOLUTION
+Cheers,Mate!
+"""
+
+age=int(input("enter your age ! "))
+x=100-age
+year=2019+x
+print("your age in ",year,"will be 100 ")
+print('------------------------------------')
+"""SOLUTION
+enter your age ! 23
+your age in 2096 will be 100
+"""
+height=int(input("enter the height of triangle? "))
+base=int(input("enter the base of triangle ?"))
+area=0.5*base*height
+print("the area of Traingle is : ",area)
+print('------------------------------------')
+"""SOLUTION 
+enter the height of triangle ? 50
+enter the base of triangle ?  20
+the area of Traingle is :  500.0
+"""
+
+
+x=11
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(abs(x*-1))
+print(int(x))
+print(float(x))
+print(divmod(x,y))
+print(pow(x,y))
+print(x**y)
+print(x>y)
+print(x==y)
+print(x!=y)
+
+"""RESULT
+14
+8
+33
+3.6666666666666665
+3
+2
+11
+11
+11.0
+(3, 2)
+1331
+1331
+True
+False
+True
+
+"""
+
+
+
+
+
